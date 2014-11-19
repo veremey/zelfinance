762f493b9c666289d27bb0b6e2453a6b6a7aa7a7
Сергей <Veremey@i.ua>
2014-11-19 23:57:55 +0200

    cmp

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
+/*	$('.language a').click(function() {
+		$(this).siblings().find('.active').removeClass('active');
+		// $(this).addClass('active');
+	});
+	return false
+*/
+	// ##############################
 
+// variant 1  - robo4iy
+
+		$('.language a').click(function() {
+		if ($(this).hasClass('is-active')) {
+			// $(this).prev().removeClass('active');
+			// $(this).next().removeClass('active');
+			// $(this).siblings().removeClass('active');
+
+		}
+		else {$(this).addClass('is-active')};
+					$(this).siblings().removeClass('is-active');
+	});
+	return false
 });
